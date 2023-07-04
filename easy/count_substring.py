37d491dd5f6a856b265a48738e7d5e7e729c7a89
Gauravesh Sharma <gauravesh.is20@bmsce.ac.in>
2023-07-05 00:24:24 +0530

    Create count_substring.py
    In this challenge, the user enters a string and a substring. You have to print the number of times that the substring occurs in the given string. String traversal will take place from left to right, not from right to left.
    
    NOTE: String letters are case-sensitive.

---
+++ easy/count_substring.py
@@ -0,0 +1,16 @@
+def count_substring(string, sub_string):
+    c=0
+    for i in range(0,len(string)):
+        modify=string[i:len(sub_string)+i]
+        #print(modify)
+        if (modify==sub_string):
+           #print(sub_string)
+           c=c+1
+    return c
+
+if __name__ == '__main__':
+    string = input().strip()
+    sub_string = input().strip()
+    
+    count = count_substring(string, sub_string)
+    print(count)
