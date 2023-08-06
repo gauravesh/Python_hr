d21ca52659929be9f8ae07b144b19abd09b0c5ef
Gauravesh Sharma <gauravesh.is20@bmsce.ac.in>
2023-08-06 21:53:02 +0530

    Create Polar Coordinates.py
    Task
    You are given a complex . Your task is to convert it to polar coordinates.
    
    Input Format
    
    A single line containing the complex number . Note: complex() function can be used in python to convert the input as a complex number.
    
    Constraints
    
    Given number is a valid complex number

---
+++ easy/Polar Coordinates.py
@@ -0,0 +1,12 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT'
+import cmath as cs
+
+
+inn=complex(input())
+#print(inn)
+r,theta = cs.polar(inn)
+
+print(r)
+print(theta)
+
+
