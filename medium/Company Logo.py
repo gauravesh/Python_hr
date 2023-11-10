2581c3395170e7bf90764f76a264f0765dc2b755
Gauravesh Sharma <gauravesh.is20@bmsce.ac.in>
2023-11-10 13:58:07 +0530

    Update Company Logo.py

--- medium/Company Logo.py
+++ medium/Company Logo.py
@@ -6,31 +6,28 @@ import random
 import re
 import sys
 
-
-
 if __name__ == '__main__':
     s = input()
-    s=sorted(s)
-    di=dict()
+    s = sorted(s)
+    di = dict()
+
     for i in s:
         if i in di:
-            di[i]+=1
+            di[i] += 1
         else:
-            di[i]=1
-    finall=dict()
-    
-    while len(finall)<3 :
-        one=max(di.values())
-        popper=''
-        for x,y in di.items():
-            if y == one:
-                popper=x
-                finall[x]=y
-        di.pop(popper)
+            di[i] = 1
 
-    for x,y in finall.items():
-        print(x,end=' ')
-        print(y)
+    finall = dict()
     
+    for count_num in range(3):
+        mv=max(di.values())
+        for x,y in di.items():
+            if y==mv:
+                print(x,end=' ')
+                print(y)
+                di.pop(x)
+                break
         
+    
+    
     
