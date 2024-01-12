af29da1bdd59c4835ec3b9e71399c6279840f080
Gauravesh Sharma <gauravesh.is20@bmsce.ac.in>
2024-01-12 23:37:39 +0530

    Create power and mod.py

---
+++ easy/Math/power and mod.py
@@ -0,0 +1,6 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+firstInput=int(input())
+secondInput=int(input())
+thirdInput=int(input())
+print(firstInput**secondInput)
+print(pow(firstInput,secondInput,thirdInput))
