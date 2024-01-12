05ce7149c42af90a29ad352001d71b4a52cb122a
Gauravesh Sharma <gauravesh.is20@bmsce.ac.in>
2024-01-12 23:34:43 +0530

    Create Mod div.py

---
+++ easy/Math/Mod div.py
@@ -0,0 +1,7 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+userInput_A=int(input())
+userInput_B=int(input())
+a,b=(divmod(userInput_A,userInput_B))
+print(a)
+print(b)
+print(divmod(userInput_A,userInput_B))
